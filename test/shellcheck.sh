61fb0073361be83d1ac80fd19f693bdefe8c8dae
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2018-05-16 18:17:21 -0400

    Add separate script for running shellcheck to ease local testing
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

---
+++ test/shellcheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+shellcheck --version
+
+# Exclude the handlebars template files since their syntax confuses shellcheck
+# There's not much bash in them anyway
+# Exclude the following shellcheck issues since they're pervasive and innocuous:
+# https://github.com/koalaman/shellcheck/wiki/SC1090
+# https://github.com/koalaman/shellcheck/wiki/SC1091
+# https://github.com/koalaman/shellcheck/wiki/SC1117
+# https://github.com/koalaman/shellcheck/wiki/SC2148
+# https://github.com/koalaman/shellcheck/wiki/SC2034
+find . -name "*.*sh" \
+	-a \! -path "*_template_plan.sh" \
+	-a \! -path "./test/integration/helpers.bash" \
+	| xargs shellcheck --external-sources --exclude=1090,1091,1117,2148,2034
+
+# This is a BATS file, so we need to override the interpreter
+# See: https://github.com/koalaman/shellcheck/issues/709
+shellcheck --shell=bash --exclude=1008 test/integration/helpers.bash

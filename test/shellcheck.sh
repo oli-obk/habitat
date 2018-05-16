201e0523859cbdd637d15d4da4aac544fdbe14e6
Jon Bauman <5906042+baumanj@users.noreply.github.com>
2018-05-16 18:36:58 -0400

    Exclude BATS helper library submodules from shellcheck run
    Signed-off-by: Jon Bauman <5906042+baumanj@users.noreply.github.com>

--- test/shellcheck.sh
+++ test/shellcheck.sh
@@ -5,7 +5,10 @@ set -euo pipefail
 shellcheck --version
 
 # Exclude the handlebars template files since their syntax confuses shellcheck
-# There's not much bash in them anyway
+# There's not much bash in them anyway.
+#
+# Exclude the bats submodules since we don't own that code.
+#
 # Exclude the following shellcheck issues since they're pervasive and innocuous:
 # https://github.com/koalaman/shellcheck/wiki/SC1090
 # https://github.com/koalaman/shellcheck/wiki/SC1091
@@ -15,8 +18,13 @@ shellcheck --version
 find . -name "*.*sh" \
 	-a \! -path "*_template_plan.sh" \
 	-a \! -path "./test/integration/helpers.bash" \
+	-a \! -path "./test/integration/test_helper/bats-assert/*" \
+	-a \! -path "./test/integration/test_helper/bats-file/*" \
+	-a \! -path "./test/integration/test_helper/bats-support/*" \
 	| xargs shellcheck --external-sources --exclude=1090,1091,1117,2148,2034
 
 # This is a BATS file, so we need to override the interpreter
 # See: https://github.com/koalaman/shellcheck/issues/709
 shellcheck --shell=bash --exclude=1008 test/integration/helpers.bash
+
+echo "shellcheck found no errors"
